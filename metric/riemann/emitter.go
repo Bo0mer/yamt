14b9c09b9733cf565c4a2a7c81866d88e7d0ef4d
Ivan Borshukov <bo0merzzz@gmail.com>
2016-06-14 08:22:29 +0000

    riemann: implement emitter
    For now depending on goryman.

--- metric/riemann/emitter.go
+++ metric/riemann/emitter.go
@@ -1,21 +1,102 @@
 package riemann
 
-import (
-	"fmt"
-)
+import "github.com/bigdatadev/goryman"
 
+// Event repesents riemann metric event.
 type Event struct {
 	Name  string
 	Value int
 }
 
-type Emitter struct{}
+type Option func(e *Emitter)
 
-func (e *Emitter) Emit(event Event) error {
-	fmt.Printf("%#v\n", event)
-	return nil
+// Prefix sets prefix to be prepended to each event name.
+func Prefix(prefix string) Option {
+	return func(e *Emitter) {
+		e.prefix = prefix
+	}
 }
 
+// Host sets the reported host for each event. Defaults to os.Hostname.
+func Host(host string) Option {
+	return func(e *Emitter) {
+		e.host = host
+	}
+}
+
+// Attributes sets attributes to be added to each emitted event.
+func Attributes(attributes map[string]string) Option {
+	return func(e *Emitter) {
+		e.attributes = attributes
+	}
+}
+
+// Tags sets tags to be added to each emitted event.
+func Tags(tags []string) Option {
+	return func(e *Emitter) {
+		e.tags = tags
+	}
+}
+
+// Emitter sends events to Riemann.
+type Emitter struct {
+	c           *goryman.GorymanClient
+	isConnected bool
+	err         error
+
+	prefix     string
+	host       string
+	attributes map[string]string
+	tags       []string
+}
+
+// NewEmitter returns brand new emitter.
+func NewEmitter(addr string, opts ...Option) *Emitter {
+	c := goryman.NewGorymanClient(addr)
+	e := &Emitter{
+		c:           c,
+		isConnected: false,
+	}
+
+	for _, opt := range opts {
+		opt(e)
+	}
+	return e
+}
+
+// Emit sends the specified event to riemann.
+func (e *Emitter) Emit(event Event) error {
+	if e.err != nil {
+		return e.err
+	}
+
+	if !e.isConnected {
+		if e.err = e.c.Connect(); e.err != nil {
+			return e.err
+		}
+		e.isConnected = true
+	}
+
+	e.err = e.c.SendEvent(&goryman.Event{
+		Service:    prependPrefix(event.Name, e.prefix),
+		Metric:     event.Value,
+		Host:       e.host,
+		Attributes: e.attributes,
+		Tags:       e.tags,
+	})
+	return e.err
+}
+
+// Err reads and resets the last error occurred during emit.
 func (e *Emitter) Err() error {
-	return nil
+	err := e.err
+	e.err = nil
+	return err
+}
+
+func prependPrefix(service string, prefix string) string {
+	if prefix == "" {
+		return service
+	}
+	return prefix + "." + service
 }
---
+++ metric/riemann/emitter_test.go
@@ -0,0 +1,86 @@
+package riemann
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPrefix(t *testing.T) {
+	prefix := "woho"
+	e := NewEmitter("", Prefix(prefix))
+	if e.prefix != prefix {
+		t.Errorf("expected prefix %q, got %q\n", prefix, e.prefix)
+	}
+}
+
+func TestHost(t *testing.T) {
+	host := "local"
+	e := NewEmitter("", Host(host))
+	if e.host != host {
+		t.Errorf("expected host %q, got %q\n", host, e.host)
+	}
+}
+
+func TestAttributes(t *testing.T) {
+	attr := map[string]string{"attr1": "value1", "x": "y"}
+	e := NewEmitter("", Attributes(attr))
+	if len(attr) != len(e.attributes) {
+		t.Errorf("expected attributes %v, got %v\n", attr, e.attributes)
+	}
+	for k, v := range attr {
+		if actualValue, ok := e.attributes[k]; !ok || v != actualValue {
+			t.Errorf("expected attributes %v, got %v\n", attr, e.attributes)
+		}
+	}
+}
+
+func TestTags(t *testing.T) {
+	tags := []string{"hash", "tag"}
+	e := NewEmitter("", Tags(tags))
+	if len(tags) != len(e.tags) {
+		t.Errorf("expected tags %v, got %v\n", tags, e.tags)
+	}
+	for i, tag := range tags {
+		if tag != e.tags[i] {
+			t.Errorf("expected tags %v, got %v\n", tags, e.tags)
+		}
+	}
+}
+
+func TestEmitDoesNothingOnErr(t *testing.T) {
+	err := errors.New("boom!")
+	e := &Emitter{
+		err: err,
+	}
+	actualErr := e.Emit(Event{})
+	if err != actualErr {
+		t.Errorf("expected err %v, got %v\n", err, actualErr)
+	}
+}
+
+func TestErrResetsState(t *testing.T) {
+	err := errors.New("boom!")
+	e := &Emitter{
+		err: err,
+	}
+	actualErr := e.Err()
+	if err != actualErr {
+		t.Errorf("expected err %v, got %v\n", err, actualErr)
+	}
+	if e.err != nil {
+		t.Errorf("expected preserved error to be nil, got %v\n", e.err)
+	}
+}
+
+func TestPrependPrefix(t *testing.T) {
+	srv, prefix := "service", "some"
+	got := prependPrefix(srv, prefix)
+	if got != "some.service" {
+		t.Errorf("expected 'some.service', got: %q\n", got)
+	}
+	prefix = ""
+	got = prependPrefix(srv, prefix)
+	if got != "service" {
+		t.Errorf("expected 'service', got: %q\n", got)
+	}
+}
