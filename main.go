76c3cddce42f123b90e7fe7a8f8614b16283782d
Ivan Borshukov <bo0merzzz@gmail.com>
2016-06-14 08:22:55 +0000

    main: add riemann config flags

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"regexp"
+	"time"
 
+	"github.com/bo0mer/yamt/metric/riemann"
 	"github.com/bo0mer/yamt/netstat"
 )
 
+var (
+	host      string
+	port      int
+	eventHost string
+)
+
+func init() {
+	flag.StringVar(&host, "h", "localhost", "Riemann host (shorthand)")
+	flag.StringVar(&host, "host", "localhost", "Riemann host")
+	flag.IntVar(&port, "p", 5555, "Riemann port (shorthand)")
+	flag.IntVar(&port, "port", 5555, "Riemann port")
+	flag.StringVar(&eventHost, "e", "", "Event hostname (shorthand)")
+	flag.StringVar(&eventHost, "event-host", "", "Event hostname")
+}
+
 func main() {
-	fmt.Println(netstat.ReadIfStats())
+	flag.Parse()
+
+	emitter := riemann.NewEmitter(fmt.Sprintf("%s:%d", host, port),
+		riemann.Host(eventHost))
+
+	re := regexp.MustCompile("lo")
+	r := netstat.NewReporter(emitter,
+		netstat.Interval(time.Second),
+		netstat.Except(re))
+	r.Start()
+	time.Sleep(time.Second * 30)
+	r.Close()
 }
